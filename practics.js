06097f847bbcd6f717aff20dd7fd4453c9f0ea89
Branden M <wasabi.dev@gmail.com>
2021-07-03 19:38:26 -0500

    1

---
+++ practics.js
@@ -0,0 +1,85 @@
+// === - Equality Operator
+// !== - Not Equal operator
+// >, <, >=, <= - Works as well
+// Logical AND Operator && - both sides are true
+// Logical OR Operator || - either side is true
+// { CODE BLOCK }
+
+// Learning
+console.log('SECTION 4:28')
+console.log('OBJECT BASICS')
+console.log('==== ==== ==== ==== ==== ==== ==== ====')
+console.log(``)
+
+let myBook = {
+    title : '1984',
+    author : 'George Orwell',
+    pageCount : 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// Challenge
+console.log('')
+console.log('CHALLENGE AREA')
+console.log('==== ==== ==== ==== ==== ==== ==== ====')
+
+// person name, age location
+let myPerson = {
+    name     : 'Ben',
+    age      :  77,
+    location : 'New England'
+}
+
+console.log(`${myPerson.name} is ${myPerson.age} and lives in ${myPerson.location}`)
+myPerson.age = myPerson.age + 33
+console.log(`${myPerson.name} is ${myPerson.age} and lives in ${myPerson.location}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('')
