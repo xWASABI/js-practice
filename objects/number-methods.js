4cc2c916c919f06a91b27ae34222d9a0404a4073
Branden M <wasabi.dev@gmail.com>
2021-07-04 17:28:15 -0500

    Number Methods

--- objects/number-methods.js
+++ objects/number-methods.js
@@ -3,13 +3,31 @@ console.log('OBJECT REFERENCES')
 console.log('==== ==== ==== ==== ==== ==== ==== ====')
 console.log(``)
 
+// Variable
 let num = 103.941
-
 // Change Decimal placement
 console.log(num.toFixed(20))
-
+// Round
 console.log(Math.round(num))
 // Round down
 console.log(Math.floor(num))
 // Round up
 console.log(Math.ceil(num))
+// Random
+let min = 10
+let max = 20
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum)
+
+// Challenge 
+// 1 - 5 = true if correct, otherwise false
+let makeGuess = function (randomNumber){
+    let min = 1
+    let max = 5
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+    return randomNumber === randomNum
+}
+
+
+console.log(makeGuess(3))
